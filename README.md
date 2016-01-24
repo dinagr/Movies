8b1c343cc75acb2f95aac8b84b29cddc78eac33d
dinagr <DINAGR@N14655C.netafim.com>
2016-01-24 21:58:00 +0200

    Syntax changes and comments
    1. Fix line that are more than 80 characters
    2. Fix the instruction in the README file
    3. Change the comments in the files

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Movies
 
-To see the app, open the fresh_tomatoes.html file in the web browser</br>
+###Instructions to open the app
 
-This app displays movies, by clicking on the movie poster the trailer of the movie will start playing in a popup window. </br>
+1. Open the fresh_tomatoes.html file in the web browser
+</br>OR
+2. Open the enterteimant_center.py file and run it.
+
+This app displays movies, by clicking a movie poster the trailer of the movie will start playing in a popup window. </br>
 
 ### This app was developed using the folowing technologies
 
--- media.py
+++ media.py
@@ -1,8 +1,8 @@
 import webbrowser
 
-#New class Movie which containes data regarding the movie.
-#The class has an init function which recives 4 parameters and adds them to the instance of the movie class
-#The class also contains a finction showtriler which plays the trailer in the webbrowser
+# New class Movie which containes data regarding the movie
+# The class has an init function which recives 4 parameters
+# The class also contains a function show_triler which plays the trailer
 class Movie():
     def __init__(self, movie_title, story_line, poster_image, trailer_youtube):
         self.title = movie_title
